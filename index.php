e51eecae3be841c60526c8c2e7a7a11e941d8427
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2018-02-02 14:02:23 +0100

    goede php complete

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	$name = "Luuk";
+	$kaasbaas = "baasvankazen";
+	if ($name=="Luuk") {
+		//echo "Luuk is da best";
+	}
+	for ($i=0; $i <= 10; $i++) { 
+		//echo $i . ", ";
+	}
+
+	date_default_timezone_set("Europe/Amsterdam");
+	
+
+	$hour = date("H");
+
+	$time = date("H:i");
+
+	if ($hour < 6) {
+		$class = "nacht";
+	}
+
+	if ($hour >=6 && $hour <12) {
+		$class = "morgen";
+	}
+
+	if ($hour >=12 && $hour <18) {
+		$class = "middag";
+	}
+
+	if ($hour >=18) {
+		$class = "avond";
+	}
+	
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="backgrounds.css">
+		<title>het uur is: <?=$hour?></title>
+	</head>
+	<body class="<?php echo $class; ?>">
+		<h1>Goede <?=$class?></h1>
+		<h2>het is nu <?=$time?></h2>
+	</body>
+</html>
